f0bd8df5041bfe93f0bbafda4aef6b4424a8785d
VladislavOoll <apahiphip@mail.ru>
2021-03-28 18:10:49 +0300

    fix: count cats (delite test) feat: Carbon dating code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY= 15; // 15
+const HALF_LIFE_PERIOD= 5730; //k
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    let result = +sampleActivity > 0 &&
+    +sampleActivity <= 15 &&
+    typeof sampleActivity === "string"? +sampleActivity : false;
+    return result?
+        Math.ceil(Math.log(MODERN_ACTIVITY/result)/(0.693/HALF_LIFE_PERIOD)) : false;
 };
